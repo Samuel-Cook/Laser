2c1f052e67fb8333eff2fa63b4bd16cfbf143d47
Samuel Cook <s172776@ucs.ac.uk>
2018-02-07 08:02:40 +0000

    many changes

---
+++ Assets/Scripts/fireSplash.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireSplash : MonoBehaviour
+{
+    public bool waitTime;
+
+    void Start()
+    {
+        waitTime = true;
+        StartCoroutine("LaserFire");
+    }
+    void Update()
+
+    {
+        //if(waitTime != 0)
+        {
+            //waitTime = waitTime - 1*Time.deltaTime;
+            
+        }
+    }
+
+    IEnumerator LaserFire()
+    {
+        while (waitTime == true)
+        {
+            transform.Translate(Time.deltaTime, 0, 0, Space.World);
+
+            yield return null;
+        }
+    }
+}
