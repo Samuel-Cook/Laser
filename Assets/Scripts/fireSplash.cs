eb6c96804685cacfdfb4befabfdcfa14353670e4
None <None>
2018-02-07 15:31:02 +0000

    add

--- Assets/Scripts/fireSplash.cs
+++ Assets/Scripts/fireSplash.cs
@@ -5,25 +5,30 @@ using UnityEngine;
 public class fireSplash : MonoBehaviour
 {
     public bool waitTime;
+    public float wait;
 
     void Start()
     {
         waitTime = true;
+        wait = 0;
         StartCoroutine("LaserFire");
     }
     void Update()
 
     {
-        //if(waitTime != 0)
+        if (waitTime = true)
         {
-            //waitTime = waitTime - 1*Time.deltaTime;
-            
+            wait = wait + 1 * Time.deltaTime;
+        }
+        if (wait >= 12)
+        {
+            waitTime = false;
         }
     }
 
     IEnumerator LaserFire()
     {
-        while (waitTime == true)
+        while (waitTime == true && wait <= 12)
         {
             transform.Translate(Time.deltaTime, 0, 0, Space.World);
 
